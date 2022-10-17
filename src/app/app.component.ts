c2ea58241cf7ee8bd7ac6d2856298af176c25e9c
unknown <scundanomine21@gmail.com>
2022-10-17 19:57:19 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MRFP-22-23-007-Rev-00-TDF';
+}
