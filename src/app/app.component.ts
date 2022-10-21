4767402d4ff54a8cc4c5440227e45d1a39b35481
unknown <scundanomine21@gmail.com>
2022-10-22 05:22:12 +0530

    wow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   topics=["Angular", "React", "Vue"];
 
   userModel:{name:string, email:string, phone:number, topic:string, timepreference:string, subscribe:boolean} ={name:"Sam", email:"sam@gmail.com", phone:5455484789748, topic:"Angular", timepreference:"morning", subscribe:false}
+
+  onClick(a:boolean, b:boolean, fieldName:string){
+      if(a && b)
+        alert(`Pleased enter ${fieldName}`)
+  }
 }
